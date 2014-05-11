99abf1013c9244ca954ba647f8343f9bc1b7a92a
lziegelmeier <lziegel1@macalester.edu>
2014-05-11 12:07:27 -0500

    Plotting Assignment

--- plot1.R
+++ plot1.R
@@ -2,7 +2,7 @@
 file<-"./data/household_power_consumption.txt"
 hpc<-read.csv(file,header=TRUE,sep=";",colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"),na.strings="?")
 
-##Convert Date and Time Variables
+##Convert Date Variable
 hpc$Date<-as.Date(hpc$Date, "%d/%m/%Y")
 ##Subset data from the dates 2007-02-01 and 2007-02-02
 hpcNew<-subset(hpc,Date>="2007-02-01" & Date<="2007-02-02")
